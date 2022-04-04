8b6b3644777da20619a1350a4f3d3f5129a8cb36
livelovebll <70596067+livelovebll@users.noreply.github.com>
2022-04-04 14:18:48 -0600

    all

---
+++ 2022_04_04_195332_create_watched_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watched', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('people_id')->references('id')->on('people');
+            $table->foreign('movie_id')-> references('id')->on('movies');
+            $table->string('stars');
+            $table->string('comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('watched');
+    }
+};
